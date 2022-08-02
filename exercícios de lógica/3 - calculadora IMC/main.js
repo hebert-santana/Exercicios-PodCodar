d275f1871517bca8296dff1d7cdf627a02f2db4c
hebert-santana <hebert.fsantana@gmail.com>
2022-08-02 11:55:35 -0300

    problema 3 (final)

--- exercícios de lógica/3 - calculadora IMC/main.js
+++ exercícios de lógica/3 - calculadora IMC/main.js
@@ -1,32 +1,31 @@
-function enviarForm() {
-
+document.querySelector('#form').addEventListener('submit', function(e) {
+    e.preventDefault();
     var nome = document.querySelector('#form__nome').value;
     const peso = Number(document.querySelector('#form__peso').value);
     const altura = Number(document.querySelector('#form__altura').value);
     var sexo = document.querySelector('input[name="sexo"]:checked').value;
     var imc = (peso / (Math.pow((altura/100), 2))).toFixed(1);
-    var texto = '';
+    let resultado = document.querySelector('.resultado');
 
     if (sexo == 1 || sexo == 2) {
         if (sexo == 1) {
-            if (imc < 20.7) {texto = 'Seu IMC é: ' + imc + '.\nVocê está abaixo do peso; coma pizza.'}
-            else if (imc >= 20.7 && imc < 26.4) { texto = 'Seu IMC é: ' + imc + '.\nVocê está no peso normal.'}
-            else if (imc >= 26.4 && imc < 27.8) { texto = 'Seu IMC é: ' + imc + '.\nVocê está um pouco acima do peso; corte os doces.'}
-            else if (imc >= 27.8 && imc < 31.1) { texto = 'Seu IMC é: ' + imc + '.\nVocê está pesadinho, faça academia e coma menos.'}
-            else if (imc >= 31.1) {texto = 'Seu IMC é: ' + imc + '.\nVocê está GORDO!'}
+            if (imc < 20.7) {resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está abaixo do peso; coma pizza.'}
+            else if (imc >= 20.7 && imc < 26.4) { resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está no peso normal.'}
+            else if (imc >= 26.4 && imc < 27.8) { resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está um pouco acima do peso; corte os doces.'}
+            else if (imc >= 27.8 && imc < 31.1) { resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está pesadinho, faça academia e coma menos.'}
+            else if (imc >= 31.1) {resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está GORDO!'}
         }
         // se for do sexo feminino
         else if (sexo == 2) {
-            if (imc < 19.1) { texto = 'Seu IMC é: ' + imc + '.\nVocê está abaixo do peso; coma pizza.'}    
-            else if (imc >= 19.1 && imc < 25.8) { texto = 'Seu IMC é: ' + imc + '.\nVocê está no peso normal.'}
-            else if (imc >= 25.8 && imc < 27.3) { texto = 'Seu IMC é: ' + imc + '.\nVocê está um pouco acima do peso; corte os doces.'}
-            else if (imc >= 27.3 && imc < 32.3) { texto = 'Seu IMC é: ' + imc + '.\nVocê está pesadinha, faça academia e coma menos.'}
-            else if (imc >= 32.3) { texto = 'Seu IMC é: ' + imc + '.\nVocê está GORDA!'}
+            if (imc < 19.1) { resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está abaixo do peso; coma pizza.'}    
+            else if (imc >= 19.1 && imc < 25.8) { resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está no peso normal.'}
+            else if (imc >= 25.8 && imc < 27.3) { resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está um pouco acima do peso; corte os doces.'}
+            else if (imc >= 27.3 && imc < 32.3) { resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está pesadinha, faça academia e coma menos.'}
+            else if (imc >= 32.3) { resultado.innerHTML = 'Seu IMC é: ' + imc + '.\nVocê está GORDA!'}
         }
         else {
-            texto = 'Houston, we have a problem! Reinicie a aplicação e tente novamente.'
+            resultado.innerHTML = 'Houston, we have a problem! Reinicie a aplicação e tente novamente.'
         }
     }
-    alert('Olá, ' + nome + '.\n' + texto +'.');
-}
+});
 
