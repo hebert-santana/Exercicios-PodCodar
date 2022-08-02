973454abd7f05355d7152dc666fe2d52acfe2609
hebert-santana <hebert.fsantana@gmail.com>
2022-08-02 11:20:58 -0300

    manipulando DOM

--- exercícios de lógica/4 - apólice de seguro/main.js
+++ exercícios de lógica/4 - apólice de seguro/main.js
@@ -7,31 +7,32 @@ function enviarForm() {
     const hJovem = 1.07;
     const idoso = 1.09;
     const mulher = 0.95;
+    let resultado = document.querySelector('.resultado')
    
 
     if (idade == 4) {
-        alert("Lamentamos informar, mas talvez você já tenha falecido.")
+        resultado.innerHTML = "Lamentamos informar, mas talvez você já tenha falecido."
     } else if (idade == 3) {
-        if (cobertura == 500) { var preco = cobertura * idoso; alert('Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.')}   
-        else if (cobertura == 800) { var preco = cobertura * idoso; alert('Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.')}
+        if (cobertura == 500) { var preco = cobertura * idoso; resultado.innerHTML = 'Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.'}   
+        else if (cobertura == 800) { var preco = cobertura * idoso; resultado.innerHTML = 'Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.'}
     }
 
     if (sexo == 1) {
         if (idade == 1) {
-            if (cobertura == 500) { var preco = cobertura * hJovem; alert('Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.')}   
-            else if (cobertura == 800) { var preco = cobertura * hJovem; alert('Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.')}
+            if (cobertura == 500) { var preco = cobertura * hJovem; resultado.innerHTML = 'Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.'}   
+            else if (cobertura == 800) { var preco = cobertura * hJovem; resultado.innerHTML = 'Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.'}
         }
         else if (idade == 2) {
-            if (cobertura == 500) { var preco = cobertura; alert('Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.')}   
-            else if (cobertura == 800) { var preco = cobertura; alert('Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.')}
+            if (cobertura == 500) { var preco = cobertura; resultado.innerHTML = 'Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.'}   
+            else if (cobertura == 800) { var preco = cobertura; resultado.innerHTML = 'Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.'}
         }
        
     }
 
     else if (sexo == 2) {
         if (idade == 1 || idade == 2) {
-            if (cobertura == 500) { var preco = cobertura * mulher; alert('Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.')}   
-            else if (cobertura == 800) { var preco = cobertura * mulher; alert('Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.')}
+            if (cobertura == 500) { var preco = cobertura * mulher; resultado.innerHTML = 'Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.'}   
+            else if (cobertura == 800) { var preco = cobertura * mulher; resultado.innerHTML = 'Olá, ' + nome + '.\nO valor da cobertura para você é: R$' + preco + '.'}
         }
     }
 }
